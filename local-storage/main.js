7f05e5ed876b611753901dc134d5f47cb2290427
Rianne <9632612+littlepandabear@users.noreply.github.com>
2023-11-03 18:16:28 -0600

    Update main.js

--- local-storage/main.js
+++ local-storage/main.js
@@ -7,10 +7,43 @@ if (typeof(Storage) !== "undefined") {
 	var storage = localStorage.getItem("data");
 	
 	document.body.innerHTML = storage;
+
+
+
+	//store complex data with objects or arrays
+	const user = {
+		user: "username",
+		score: 100
+	}
+
+	localStorage.setItem("user", JSON.stringify(user)); 
+	const userObj = JSON.parse(localStorage.getItem("getItem"));
+	console.log(userObj)
+
+
+	localStorage.setItem("arr", JSON.stringify([1,2,3])); 
+	const arr = JSON.parse(localStorage.getItem("arr"));
+	console.log(arr)
+
+	//delete storage by key or all keys 
+	deleteStorage("arr")
 	
 	
 } else {
     // No Web Storage support
 	document.body.innerHTML = "local storage is not suported";
 	
+}
+
+//delete localstorage
+function deleteStorage(key = null){
+
+	if (key!= null){
+		//delete by key
+		localStorage.removeItem(key);	
+	} else {
+		//delete all
+		localStorage.clear();	
+	}
+
 }
